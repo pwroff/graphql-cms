9f109efc9f6fdcaaa97ee9e5206236ab85d3c739
Leonid Lazaryev <leonidlazaryev@gmail.com>
2017-02-06 23:39:14 +0100

    added pagination to resolvers

--- src/server/api/resolvers.js
+++ src/server/api/resolvers.js
@@ -6,24 +6,33 @@ import Division from '../data/models/Division';
 
 import log from '../../log'
 
+const withPagination = (executor, {skip = 0, limit = 10, offset = 0}) => {
+    return executor()
+        .limit(limit + offset)
+        .skip(skip)
+        .sort({
+            created: 'asc'
+        });
+};
+
 const resolvers = {
     Query: {
-        items(_, args) {
-            return Item.find(args);
+        items(_, {pagination}) {
+            return withPagination(Item.find.bind(Item), pagination);
         },
         item(_, {id}) {
             return Item.findById(id);
         },
-        divisions(_, args) {
-            return Division.find();
+        divisions(_, {pagination}) {
+            return withPagination(Division.find.bind(Division), pagination);
         },
         division(_, {id}) {
             return Division.findById(id);
         }
     },
     Mutation: {
-        createDivision(_, {title, description}) {
-            const d = new Division({title, description, created: Date.now()});
+        createDivision(_, {params}) {
+            const d = new Division(params);
             return new Promise((res, rej)=>{
                 d.save((e, data)=>{
                     if (e) {
@@ -35,8 +44,8 @@ const resolvers = {
                 })
             });
         },
-        createItem(_, props) {
-            const i = new Item(props);
+        createItem(_, {params}, context) {
+            const i = new Item(params);
             return new Promise((res, rej)=>{
                 i.save((e, data)=>{
                     if (e) {
